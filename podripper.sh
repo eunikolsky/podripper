a56eef2395ee3c569bd53be11420f5ce8fb17b84
Eugene Nikolsky <eunikolsky@gmail.com>
2019-07-10 18:38:11 -0700

    When reencoding fails, move the original file instead of linking it

--- podripper.sh
+++ podripper.sh
@@ -57,11 +57,11 @@ if [[ -n "$( ls -A "$RIP_OUTPUT_DIR" )" ]]; then
   for rip in "$RIP_OUTPUT_DIR"/*.mp3; do
     pod_title="$( echo "$rip" | sed -nE 's/.*([0-9]{4})_([0-9]{2})_([0-9]{2})_([0-9]{2})_([0-9]{2})_([0-9]{2}).*/\1-\2-\3 \4:\5:\6/p' )"
     if ! ffmpeg -hide_banner -i "$rip" -vn -v warning -codec:a libmp3lame -q:a 4 -metadata title="$pod_title" -metadata artist="$POD_ARTIST" -metadata album="$POD_ALBUM" -metadata date="$year" -metadata genre=Podcast "$ENC_RIP_OUTPUT_DIR/$( basename -s .mp3 "$rip" )_enc.mp3"; then
-      echo "reencoding $rip failed; linking the source"
-      ln "$rip" "$ENC_RIP_OUTPUT_DIR/$( basename -s .mp3 "$rip" )_src.mp3"
+      echo "reencoding $rip failed; moving the source"
+      mv "$rip" "$ENC_RIP_OUTPUT_DIR/$( basename -s .mp3 "$rip" )_src.mp3"
+    else
+      trash-put "$rip"
     fi
-
-    trash-put "$rip"
   done
 else
   echo "no files in $RIP_OUTPUT_DIR"
