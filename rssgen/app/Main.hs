fe55fd1a9020f2916403df2cc575aa593a4396c9
Eugene <e@egeek.me>
2020-03-25 21:02:03 +0200

    List the mp3 files from a separate directory

--- rssgen/app/Main.hs
+++ rssgen/app/Main.hs
@@ -8,7 +8,9 @@ main :: IO ()
 main = shakeArgs shakeOptions $ do
   want ["radiot" <.> "rss"]
 
-  versioned 1 $ "radiot" <.> "rss" %> \out -> do
-    -- we need the audio files to generate the RSS
-    mp3Files <- getDirectoryFiles "" ["*.mp3"]
+  versioned 3 $ "radiot" <.> "rss" %> \out -> do
+    -- we need the audio files to generate the RSS, which are in the
+    -- directory of the same name as the podcast title
+    let mp3Directory = dropExtension out
+    mp3Files <- getDirectoryFiles "" [mp3Directory </> "*.mp3"]
     writeFile' out $ mconcat ["<!-- ", intercalate ", " mp3Files, " -->"]
