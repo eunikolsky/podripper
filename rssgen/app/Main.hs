30204c38a8b2d8a2eb433aaad995aea4fa07a7cb
Eugene <e@egeek.me>
2020-03-28 14:02:18 +0200

    Extract feed values to `RSSFeedConfig`

--- rssgen/app/Main.hs
+++ rssgen/app/Main.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
 import Data.List
@@ -8,14 +10,24 @@ import Development.Shake.FilePath
 import RSSFeed
 import RSSItem
 
+feedConfig :: RSSFeedConfig
+feedConfig = RSSFeedConfig
+  { RSSFeed.title = "Поток"
+  , RSSFeed.description = "foo"
+  , RSSFeed.language = "ru"
+  , RSSFeed.podcastLink = "https://example.com/"
+  , RSSFeed.imageLink = "https://example.com/cover.jpg"
+  , RSSFeed.selfLink = "https://example.com/sample.rss"
+  }
+
 main :: IO ()
 main = shakeArgs shakeOptions $ do
   want ["radiot" <.> "rss"]
 
-  versioned 5 $ "radiot" <.> "rss" %> \out -> do
+  versioned 6 $ "radiot" <.> "rss" %> \out -> do
     -- we need the audio files to generate the RSS, which are in the
     -- directory of the same name as the podcast title
     let mp3Directory = dropExtension out
     mp3Files <- getDirectoryFiles "" [mp3Directory </> "*.mp3"]
     rssItems <- liftIO . fmap catMaybes $ traverse rssItemFromFile mp3Files
-    writeFile' out $ feed rssItems
+    writeFile' out $ feed feedConfig rssItems
--- rssgen/src/RSSFeed.hs
+++ rssgen/src/RSSFeed.hs
@@ -1,12 +1,25 @@
 module RSSFeed where
 
+import qualified Data.Text as T
 import Text.XML.Light
 
 import RSSItem
 
+-- | Values for the RSS feed tags.
+data RSSFeedConfig = RSSFeedConfig
+  { title :: T.Text
+  , description :: T.Text
+  , language :: T.Text
+  , podcastLink :: T.Text
+  , imageLink :: T.Text
+  , selfLink :: T.Text
+  }
+  deriving (Show)
+
 -- | Returns the XML string of the entire RSS feed with the RSS items.
-feed :: [RSSItem] -> String
-feed rssItems = ppcElement config rss
+feed :: RSSFeedConfig -> [RSSItem] -> String
+feed (RSSFeedConfig fcTitle fcDescription fcLanguage fcPodcastLink fcImageLink fcSelfLink) rssItems =
+  ppcElement config rss
   where
     config = useShortEmptyTags (/= unqual "description") prettyConfigPP
 
@@ -15,16 +28,18 @@ feed rssItems = ppcElement config rss
     xmlns_atom = Attr (QName atomns_name Nothing (Just "xmlns")) "http://www.w3.org/2005/Atom"
     channel = unode "channel" $ [link, title, language, description, generator, image, atom_link] <> items
 
-    link = unode "link" "https://example.com/"
-    title = unode "title" "Поток"
-    language = unode "language" "ru"
-    description = unode "description" "foo"
+    link = unodet "link" fcPodcastLink
+    title = unodet "title" fcTitle
+    language = unodet "language" fcLanguage
+    description = unodet "description" fcDescription
     generator = unode "generator" "rssgen"
-    image = unode "image" $ unode "url" "https://example.com/cover.jpg"
+    image = unode "image" $ unodet "url" fcImageLink
     atom_link = node (QName "link" Nothing (Just atomns_name))
       [ Attr (unqual "rel") "self"
       , Attr (unqual "type") "application/rss+xml"
-      , Attr (unqual "href") "https://example.com/sample.rss"
+      , Attr (unqual "href") $ T.unpack fcSelfLink
       ]
 
     items = fmap renderItem rssItems
+
+    unodet name = unode name . T.unpack
