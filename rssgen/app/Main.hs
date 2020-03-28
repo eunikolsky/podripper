dbbfef5ecb4bce6cd849de055fe3836149db5ee3
Eugene <e@egeek.me>
2020-03-28 12:22:49 +0200

    Render the RSS feed with mp3 files

--- rssgen/app/Main.hs
+++ rssgen/app/Main.hs
@@ -1,18 +1,21 @@
 module Main where
 
 import Data.List
+import Data.Maybe
 import Development.Shake
 import Development.Shake.FilePath
 
 import RSSFeed
+import RSSItem
 
 main :: IO ()
 main = shakeArgs shakeOptions $ do
   want ["radiot" <.> "rss"]
 
-  versioned 4 $ "radiot" <.> "rss" %> \out -> do
+  versioned 5 $ "radiot" <.> "rss" %> \out -> do
     -- we need the audio files to generate the RSS, which are in the
     -- directory of the same name as the podcast title
     let mp3Directory = dropExtension out
-    --mp3Files <- getDirectoryFiles "" [mp3Directory </> "*.mp3"]
-    writeFile' out feed
+    mp3Files <- getDirectoryFiles "" [mp3Directory </> "*.mp3"]
+    rssItems <- liftIO . fmap catMaybes $ traverse rssItemFromFile mp3Files
+    writeFile' out $ feed rssItems
--- rssgen/src/RSSFeed.hs
+++ rssgen/src/RSSFeed.hs
@@ -2,14 +2,18 @@ module RSSFeed where
 
 import Text.XML.Light
 
--- | Returns the XML string with the entire RSS feed.
-feed :: String
-feed = ppElement rss
+import RSSItem
+
+-- | Returns the XML string of the entire RSS feed with the RSS items.
+feed :: [RSSItem] -> String
+feed rssItems = ppcElement config rss
   where
+    config = useShortEmptyTags (/= unqual "description") prettyConfigPP
+
     rss = unode "rss" ([Attr (unqual "version") "2.0", xmlns_atom], channel)
     atomns_name = "atom"
     xmlns_atom = Attr (QName atomns_name Nothing (Just "xmlns")) "http://www.w3.org/2005/Atom"
-    channel = unode "channel" [link, title, language, description, generator, image, atom_link]
+    channel = unode "channel" $ [link, title, language, description, generator, image, atom_link] <> items
 
     link = unode "link" "https://example.com/"
     title = unode "title" "Поток"
@@ -22,3 +26,5 @@ feed = ppElement rss
       , Attr (unqual "type") "application/rss+xml"
       , Attr (unqual "href") "https://example.com/sample.rss"
       ]
+
+    items = fmap renderItem rssItems
