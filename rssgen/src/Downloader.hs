3f7f26632a35d2d46bb80532b81171843754ff44
Eugene <e@egeek.me>
2020-04-28 10:37:55 +0300

    Return fake XML with the URL

--- rssgen/src/Downloader.hs
+++ rssgen/src/Downloader.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Downloader where
 
 import qualified Data.ByteString.Lazy as BL
+import qualified Data.ByteString.Lazy.Char8 as CL
 
 type URL = String
 
@@ -14,9 +16,9 @@ class Monad m => MonadDownload m where
   getFile :: URL -> m (Maybe Bytes)
 
 
--- |A fake downloader that always returns @Nothing@.
+-- |A fake downloader that always returns an XML with the commented URL.
 newtype FakeDownloadT m a = FakeDownloadT { runFakeDownloadT :: m a }
   deriving (Functor, Applicative, Monad)
 
 instance Monad m => MonadDownload (FakeDownloadT m) where
-  getFile = const $ pure Nothing
+  getFile url = pure . Just $ "<!-- " <> CL.pack url <> " -->"
