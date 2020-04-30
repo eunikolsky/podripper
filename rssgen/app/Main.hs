47b05d301efcd008b0dfef7f95c9377bc810190b
Eugene <e@egeek.me>
2020-04-30 09:15:28 +0300

    Place the shake work dir in `/var/lib/podripper/` by default
    This can be overridden with the `SHAKE_DIR` environment variable; set it
    to `.shake` for the default shake directory.

--- rssgen/app/Main.hs
+++ rssgen/app/Main.hs
@@ -14,6 +14,7 @@ import Data.Version (Version, showVersion)
 import Development.Shake
 import Development.Shake.Classes
 import Development.Shake.FilePath
+import qualified System.Environment as Env
 
 import qualified Paths_rssgen as Paths (version)
 import RSSFeed
@@ -25,34 +26,36 @@ newtype RSSGenVersion = RSSGenVersion ()
 type instance RuleResult RSSGenVersion = Version
 
 main :: IO ()
-main = shakeArgs shakeOptions { shakeColor = True } $ do
-  want $ ["radiot", "rcmp"] <&> (<.> "rss")
+main = do
+  shakeDir <- fromMaybe "/var/lib/podripper/shake" <$> Env.lookupEnv "SHAKE_DIR"
+  shakeArgs shakeOptions { shakeFiles = shakeDir, shakeColor = True } $ do
+    want $ ["radiot", "rcmp"] <&> (<.> "rss")
 
-  getRSSGenVersion <- addOracle $ \(RSSGenVersion _) -> return Paths.version
+    getRSSGenVersion <- addOracle $ \(RSSGenVersion _) -> return Paths.version
 
-  versioned 12 $ "*.rss" %> \out -> do
-    getRSSGenVersion $ RSSGenVersion ()
+    versioned 12 $ "*.rss" %> \out -> do
+      getRSSGenVersion $ RSSGenVersion ()
 
-    configDir <- getEnvWithDefault "/usr/share/podripper" "CONF_DIR"
-    let podcastTitle = dropExtension out
-        feedConfigFile = configDir </> podcastTitle <> "_feed.conf"
-    need [feedConfigFile]
-    feedConfig <- fmap decode . liftIO $ BL.readFile feedConfigFile
-    case feedConfig of
-      Just config -> generateFeed config out
-      Nothing -> fail $ "Couldn't parse feed config file " <> feedConfigFile
+      configDir <- getEnvWithDefault "/usr/share/podripper" "CONF_DIR"
+      let podcastTitle = dropExtension out
+          feedConfigFile = configDir </> podcastTitle <> "_feed.conf"
+      need [feedConfigFile]
+      feedConfig <- fmap decode . liftIO $ BL.readFile feedConfigFile
+      case feedConfig of
+        Just config -> generateFeed config out
+        Nothing -> fail $ "Couldn't parse feed config file " <> feedConfigFile
 
-    where
-      generateFeed :: RSSFeedConfig -> FilePattern -> Action ()
-      generateFeed feedConfig out = do
-        -- we need the audio files to generate the RSS, which are in the
-        -- directory of the same name as the podcast title
-        let podcastTitle = dropExtension out
-        mp3Files <- getDirectoryFiles "" [podcastTitle </> "*.mp3"]
-        rssItems <- liftIO . fmap (newestFirst . catMaybes) $ traverse (rssItemFromFile podcastTitle) mp3Files
+      where
+        generateFeed :: RSSFeedConfig -> FilePattern -> Action ()
+        generateFeed feedConfig out = do
+          -- we need the audio files to generate the RSS, which are in the
+          -- directory of the same name as the podcast title
+          let podcastTitle = dropExtension out
+          mp3Files <- getDirectoryFiles "" [podcastTitle </> "*.mp3"]
+          rssItems <- liftIO . fmap (newestFirst . catMaybes) $ traverse (rssItemFromFile podcastTitle) mp3Files
 
-        version <- askOracle $ RSSGenVersion ()
-        writeFile' out $ feed (ProgramVersion . showVersion $ version) feedConfig rssItems
+          version <- askOracle $ RSSGenVersion ()
+          writeFile' out $ feed (ProgramVersion . showVersion $ version) feedConfig rssItems
 
-      newestFirst :: [RSSItem] -> [RSSItem]
-      newestFirst = sortOn Down
+        newestFirst :: [RSSItem] -> [RSSItem]
+        newestFirst = sortOn Down
