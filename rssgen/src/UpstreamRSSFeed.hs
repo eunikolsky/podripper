f964b0415c399d4a4058a587edc14fb0997d894b
Eugene <e@egeek.me>
2020-05-04 08:49:36 +0300

    Parse upstream RSS

--- rssgen/src/UpstreamRSSFeed.hs
+++ rssgen/src/UpstreamRSSFeed.hs
@@ -1,10 +1,12 @@
 module UpstreamRSSFeed
-  ( UpstreamRSSItem
+  ( UpstreamRSSItem(..)
   , parse
   ) where
 
+import Data.List
 import qualified Data.Text as T
 import Data.Time.Clock
+import Data.Time.Format
 import Text.XML.Light
 
 -- |A podcast episode of the upstream RSS.
@@ -16,4 +18,21 @@ data UpstreamRSSItem = UpstreamRSSItem
 
 -- |Parses the upstream RSS into items.
 parse :: T.Text -> Maybe [UpstreamRSSItem]
-parse = const $ pure []
+parse = parseRSS . parseXML
+  where
+    parseRSS :: [Content] -> Maybe [UpstreamRSSItem]
+    parseRSS contents = do
+      let elements = onlyElems contents
+      rss <- find ((== unqual "rss") . elName) elements
+      channel <- findChild (unqual "channel") rss
+      let items = findChildren (unqual "item") channel
+      traverse parseRSSItem items
+
+    parseRSSItem :: Element -> Maybe UpstreamRSSItem
+    parseRSSItem element = UpstreamRSSItem
+      <$> (T.pack . strContent <$> findChild (unqual "title") element)
+      <*> (parsePubDate =<< strContent <$> findChild (unqual "pubDate") element)
+
+    parsePubDate :: String -> Maybe UTCTime
+    parsePubDate = parseTimeM acceptSurroundingWhitespace defaultTimeLocale rfc822DateFormat
+      where acceptSurroundingWhitespace = False
