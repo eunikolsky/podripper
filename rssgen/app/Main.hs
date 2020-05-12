62e3103025efbdd2e093549c76a4b408d21e9c3e
Eugene <e@egeek.me>
2020-05-12 09:41:37 +0300

    [wip] Use oracle for the upstream RSS

--- rssgen/app/Main.hs
+++ rssgen/app/Main.hs
@@ -11,8 +11,9 @@ import Data.Functor
 import Data.List
 import Data.Maybe
 import Data.Ord (Down(..))
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as TE
 import qualified Data.Text.Lazy as TL
-import qualified Data.Text.Lazy.Encoding as TLE
 import Data.Version (Version, showVersion)
 import Development.Shake
 import Development.Shake.Classes
@@ -25,11 +26,21 @@ import qualified Paths_rssgen as Paths (version)
 import Downloader
 import RSSFeed
 import RSSItem
+import qualified UpstreamRSSFeed
 
 newtype RSSGenVersion = RSSGenVersion ()
   deriving (Show, Typeable, Eq, Hashable, Binary, NFData)
-
 type instance RuleResult RSSGenVersion = Version
+
+newtype PodcastName = PodcastName String
+  deriving (Show, Eq, Hashable, Binary, NFData)
+
+-- |This oracle is to depend on the upstream RSS without an intermediate file.
+-- If the downloading failed, we don't want to fail our build.
+-- PodcastName -> Maybe T.Text
+newtype UpstreamRSS = UpstreamRSS PodcastName
+  deriving (Show, Typeable, Eq, Hashable, Binary, NFData)
+type instance RuleResult UpstreamRSS = Maybe T.Text
 
 main :: IO ()
 main = do
@@ -37,46 +48,47 @@ main = do
   shakeArgs shakeOptions { shakeFiles = shakeDir, shakeColor = True } $ do
     want $ ["radiot", "rcmp"] <&> (<.> "rss")
 
-    getRSSGenVersion <- addOracle $ \(RSSGenVersion _) -> return Paths.version
+    getRSSGenVersion <- addOracle $ \RSSGenVersion{} -> return Paths.version
 
-    versioned 12 $ "*.rss" %> \out -> do
+    upstreamRSS <- addOracle $ \(UpstreamRSS _) -> do
+      manager <- liftIO $ newManager tlsManagerSettings
+      -- this `liftIO` is to fix the build error:
+      -- • No instance for (exceptions-0.10.4:Control.Monad.Catch.MonadThrow
+      --                      Action)
+      --     arising from a use of ‘downloadRadioTRSS’
+      liftIO $ runReaderT (runHTTPClientDownloadT downloadRadioTRSS) manager
+
+    versioned 13 $ "*.rss" %> \out -> do
       getRSSGenVersion $ RSSGenVersion ()
 
       configDir <- getEnvWithDefault "/usr/share/podripper" "CONF_DIR"
       let podcastTitle = dropExtension out
           feedConfigFile = configDir </> podcastTitle <> "_feed.conf"
       need [feedConfigFile]
-      feedConfig <- fmap decode . liftIO $ BL.readFile feedConfigFile
+      feedConfig <- fmap decode . liftIO . BL.readFile $ feedConfigFile
+      -- TODO it seems that downloading is not triggered if the local RSS is already up-to-date
+      -- use a file instead of the oracle?
+      maybeUpstreamRSSBytes <- upstreamRSS . UpstreamRSS . PodcastName $ podcastTitle
       case feedConfig of
-        Just config -> generateFeed config out
+        Just config -> do
+          let maybeUpstreamRSS = maybeUpstreamRSSBytes >>= UpstreamRSSFeed.parse
+          generateFeed config (fromMaybe [] maybeUpstreamRSS) out
         Nothing -> fail $ "Couldn't parse feed config file " <> feedConfigFile
 
-    versioned 3 $ "_radiot.rss" %> \out -> do
-      alwaysRerun
-      manager <- liftIO $ newManager tlsManagerSettings
-      -- this `liftIO` is to fix the build error:
-      -- • No instance for (exceptions-0.10.4:Control.Monad.Catch.MonadThrow
-      --                      Action)
-      --     arising from a use of ‘downloadRadioTRSS’
-      maybeRSS <- liftIO $ runReaderT (runHTTPClientDownloadT downloadRadioTRSS) manager
-      case maybeRSS of
-        Just rss -> writeFileChanged out $ TL.unpack rss
-        Nothing -> putWarn $ "Can't download " <> out
+    where
+      generateFeed :: RSSFeedConfig -> [UpstreamRSSFeed.UpstreamRSSItem] -> FilePattern -> Action ()
+      generateFeed feedConfig upstreamItems out = do
+        -- we need the audio files to generate the RSS, which are in the
+        -- directory of the same name as the podcast title
+        let podcastTitle = dropExtension out
+        mp3Files <- getDirectoryFiles "" [podcastTitle </> "*.mp3"]
+        rssItems <- liftIO . fmap (newestFirst . catMaybes) $ traverse (rssItemFromFile podcastTitle upstreamItems) mp3Files
 
-      where
-        generateFeed :: RSSFeedConfig -> FilePattern -> Action ()
-        generateFeed feedConfig out = do
-          -- we need the audio files to generate the RSS, which are in the
-          -- directory of the same name as the podcast title
-          let podcastTitle = dropExtension out
-          mp3Files <- getDirectoryFiles "" [podcastTitle </> "*.mp3"]
-          rssItems <- liftIO . fmap (newestFirst . catMaybes) $ traverse (rssItemFromFile podcastTitle) mp3Files
+        version <- askOracle $ RSSGenVersion ()
+        writeFile' out $ feed (ProgramVersion . showVersion $ version) feedConfig rssItems
 
-          version <- askOracle $ RSSGenVersion ()
-          writeFile' out $ feed (ProgramVersion . showVersion $ version) feedConfig rssItems
+      newestFirst :: [RSSItem] -> [RSSItem]
+      newestFirst = sortOn Down
 
-        newestFirst :: [RSSItem] -> [RSSItem]
-        newestFirst = sortOn Down
-
-downloadRadioTRSS :: MonadDownload m => m (Maybe TL.Text)
-downloadRadioTRSS = fmap TLE.decodeUtf8 <$> getFile "https://radio-t.com/podcast.rss"
+downloadRadioTRSS :: MonadDownload m => m (Maybe T.Text)
+downloadRadioTRSS = fmap (TE.decodeUtf8 . BL.toStrict) <$> getFile "https://radio-t.com/podcast.rss"
