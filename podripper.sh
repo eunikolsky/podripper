8c39c9a47514cc521dea60c3ab338353ba86db8e
Eugene <e@egeek.me>
2022-02-18 08:43:20 +0200

    Clarify the names of raw and done rip output directories

--- podripper.sh
+++ podripper.sh
@@ -25,22 +25,22 @@ set +o allexport
 
 # The base output directory for the rips.
 BASE_OUTPUT_DIR="/srv/http"
-ENC_RIP_OUTPUT_DIR="$BASE_OUTPUT_DIR/$RIP_DIR_NAME"
+DONE_RIP_OUTPUT_DIR="$BASE_OUTPUT_DIR/$RIP_DIR_NAME"
 # The output directory for the current rip.
-RIP_OUTPUT_DIR="$ENC_RIP_OUTPUT_DIR/incomplete/"
+RAW_RIP_OUTPUT_DIR="$DONE_RIP_OUTPUT_DIR/incomplete/"
 
-[[ -d "$RIP_OUTPUT_DIR" ]] || mkdir -p "$RIP_OUTPUT_DIR"
+[[ -d "$RAW_RIP_OUTPUT_DIR" ]] || mkdir -p "$RAW_RIP_OUTPUT_DIR"
 
 cd "$BASE_OUTPUT_DIR"
 
 # remove `mp3`s older than 10 days to clean up space
-find "$ENC_RIP_OUTPUT_DIR" -depth -maxdepth 1 -type f -name '*.mp3' -mtime +10 -delete || true
+find "$DONE_RIP_OUTPUT_DIR" -depth -maxdepth 1 -type f -name '*.mp3' -mtime +10 -delete || true
 
 # at the start, figure out the duration until which keep on ripping the stream
 END_TIMESTAMP="${END_TIMESTAMP:-$( date -d "+ ${DURATION_SEC} seconds" '+%s' )}"
 
 while (( $( date '+%s' ) < "$END_TIMESTAMP" )); do
-  streamripper "$STREAM_URL" --quiet -d "$RIP_OUTPUT_DIR" -s -r -R 3 -a -A -o version -t -m 5 -M 1000 -l "$DURATION_SEC"
+  streamripper "$STREAM_URL" --quiet -d "$RAW_RIP_OUTPUT_DIR" -s -r -R 3 -a -A -o version -t -m 5 -M 1000 -l "$DURATION_SEC"
 
   # if we've run out of time, no need to sleep one more time at the end
   if (( $( date -d "+ ${RETRY_SEC} seconds" '+%s' ) < "$END_TIMESTAMP" )); then
@@ -51,22 +51,22 @@ while (( $( date '+%s' ) < "$END_TIMESTAMP" )); do
 done
 
 # after we've spent enough time ripping, reencode the files (to fix the mp3 headers and stuff)
-if [[ -n "$( ls -A "$RIP_OUTPUT_DIR" )" ]]; then
+if [[ -n "$( ls -A "$RAW_RIP_OUTPUT_DIR" )" ]]; then
   year="$( date '+%Y' )"
-  for rip in "$RIP_OUTPUT_DIR"/*.mp3; do
+  for rip in "$RAW_RIP_OUTPUT_DIR"/*.mp3; do
     pod_title="$( echo "$rip" | sed -nE 's/.*([0-9]{4})_([0-9]{2})_([0-9]{2})_([0-9]{2})_([0-9]{2})_([0-9]{2}).*/\1-\2-\3 \4:\5:\6/p' )"
-    if ! ffmpeg -hide_banner -i "$rip" -vn -v warning -codec:a libmp3lame -b:a 96k -metadata title="$pod_title" -metadata artist="$POD_ARTIST" -metadata album="$POD_ALBUM" -metadata date="$year" -metadata genre=Podcast "$ENC_RIP_OUTPUT_DIR/$( basename -s .mp3 "$rip" )_enc.mp3"; then
+    if ! ffmpeg -hide_banner -i "$rip" -vn -v warning -codec:a libmp3lame -b:a 96k -metadata title="$pod_title" -metadata artist="$POD_ARTIST" -metadata album="$POD_ALBUM" -metadata date="$year" -metadata genre=Podcast "$DONE_RIP_OUTPUT_DIR/$( basename -s .mp3 "$rip" )_enc.mp3"; then
       echo "reencoding $rip failed; moving the source"
-      mv "$rip" "$ENC_RIP_OUTPUT_DIR/$( basename -s .mp3 "$rip" )_src.mp3"
+      mv "$rip" "$DONE_RIP_OUTPUT_DIR/$( basename -s .mp3 "$rip" )_src.mp3"
     else
       rm -f "$rip"
     fi
   done
 
   # clean up after `streamripper`
-  rm -f "$RIP_OUTPUT_DIR"/*.cue
+  rm -f "$RAW_RIP_OUTPUT_DIR"/*.cue
 else
-  echo "no files in $RIP_OUTPUT_DIR"
+  echo "no files in $RAW_RIP_OUTPUT_DIR"
 fi
 
 # finally, we should update the RSS feed
