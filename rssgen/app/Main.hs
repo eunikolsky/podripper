b111e6992fb8dc023ec833426af7f779f4bde283
Eugene <e@egeek.me>
2022-02-22 19:33:33 +0200

    Extract database functions into a new module

--- rssgen/app/Main.hs
+++ rssgen/app/Main.hs
@@ -15,8 +15,6 @@ import Data.Ord (Down(..))
 import qualified Data.Text as T
 import qualified Data.Text.Encoding as TE
 import qualified Data.Text.Lazy as TL
-import Data.Time.Clock
-import Data.Time.Format
 import Data.Version (Version, showVersion)
 import Database.SQLite.Simple
 import Development.Shake
@@ -27,6 +25,7 @@ import Network.HTTP.Client.TLS
 import qualified System.Environment as Env
 
 import qualified Paths_rssgen as Paths (version)
+import Database
 import Downloader
 import RSSFeed
 import RSSItem
@@ -101,48 +100,9 @@ main = do
       newestFirst :: [RSSItem] -> [RSSItem]
       newestFirst = sortOn Down
 
--- |Opens the episodes database and ensures the `episode` table is created.
-openDatabase :: IO Connection
-openDatabase = do
-  conn <- open "episodes.sqlite"
-  execute_ conn
-    "CREATE TABLE IF NOT EXISTS episode (\
-    \ id INTEGER PRIMARY KEY ASC NOT NULL,\
-    \ podcast TEXT NOT NULL,\
-    \ title TEXT NOT NULL,\
-    \ description TEXT NOT NULL,\
-    \ guid TEXT NOT NULL,\
-    \ publishedAt INTEGER NOT NULL,\
-    \ addedAt INTEGER NOT NULL DEFAULT (strftime('%s', 'now')),\
-    \ UNIQUE (podcast, title, description, guid, publishedAt) ON CONFLICT IGNORE)"
-  pure conn
-
--- |Saves all the parsed upstream RSS items into the database. The `episode`
--- |table ignores duplicate records.
-saveUpstreamRSSItems :: Connection -> [UpstreamRSSFeed.UpstreamRSSItem] -> IO ()
-saveUpstreamRSSItems conn
-  = executeMany conn "INSERT INTO episode (podcast,title,description,guid,publishedAt) VALUES (?,?,?,?,?)"
-  -- this provides a small benefit of the initially imported items being
-  -- sorted from lower to higher episode numbers by default
-  . oldestFirst
-
-  where oldestFirst = sortOn UpstreamRSSFeed.pubDate
-
 eitherToMaybe :: Either a b -> Maybe b
 eitherToMaybe (Left _) = Nothing
 eitherToMaybe (Right x) = Just x
 
 downloadRSS :: MonadDownload m => URL -> m (Maybe T.Text)
 downloadRSS = fmap (fmap (TE.decodeUtf8 . BL.toStrict)) . getFile
-
--- | Returns an upstream RSS item closest to @time@ if it's within one day.
-closestUpstreamItemToTime :: UpstreamRSSFeed.PodcastId -> Connection -> UTCTime -> IO (Maybe UpstreamRSSFeed.UpstreamRSSItem)
-closestUpstreamItemToTime podcast conn time = do
-  r <- queryNamed conn
-    "SELECT podcast,title,description,guid,publishedAt FROM episode\
-    \ WHERE podcast = :podcast AND\
-      \ (publishedAt BETWEEN strftime('%s', :date, '-1 days') AND strftime('%s', :date, '+1 days'))\
-    \ ORDER BY abs(publishedAt - strftime('%s', :date)) ASC, publishedAt DESC\
-    \ LIMIT 1"
-    [":podcast" := podcast, ":date" := formatTime defaultTimeLocale "%F %T" time]
-  pure $ listToMaybe r
