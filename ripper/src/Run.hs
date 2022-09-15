4100e0052f40dac1b7eb79ec9a84065c66774d7c
Eugene <e@egeek.me>
2022-09-15 20:19:38 +0300

    Stream response to a file

--- ripper/src/Run.hs
+++ ripper/src/Run.hs
@@ -2,15 +2,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Run (run) where
 
-import qualified Data.Conduit.List as CL
+import Conduit
 import Network.HTTP.Simple
-import qualified RIO.ByteString as S
 
 import Import
 
 run :: RIO App ()
 run =
-  httpSink "https://httpbin.org/drip?delay=1&duration=1&numbytes=5" $ \response -> do
+  runResourceT $ httpSink "https://httpbin.org/drip?delay=1&duration=1&numbytes=5" $ \response -> do
     logInfo . displayShow . getResponseStatus $ response
 
-    CL.mapM_ (S.hPut stdout)
+    sinkFile "out"
