7e9d922051f44f789c61a20aa4f017278c91fe03
Eugene <e@egeek.me>
2022-09-15 20:08:12 +0300

    Stream sample httpbin response to `stdout`

--- ripper/src/Run.hs
+++ ripper/src/Run.hs
@@ -2,8 +2,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Run (run) where
 
+import qualified Data.Conduit.List as CL
+import Network.HTTP.Simple
+import qualified RIO.ByteString as S
+
 import Import
 
 run :: RIO App ()
-run = do
-  logInfo "We're inside the application!"
+run =
+  httpSink "https://httpbin.org/drip?delay=1&duration=1&numbytes=5" $ \response -> do
+    logInfo . displayShow . getResponseStatus $ response
+
+    CL.mapM_ (S.hPut stdout)
