46ddb95cc46640a23d829a52e6434b6c9d18efaa
Eugene <e@egeek.me>
2023-05-13 11:45:17 +0300

    Fix not recording of non-ATP streams
    The logic in the script was incorrect: it always used the ATP's status
    before starting a recording, which is absolutely wrong for non-ATP
    streams. Thus, the RCMP recording never started.
    
    This change is to keep the livestream checks for ATP, and for other
    streams, set the `STREAM_IS_LIVE` flag directly.

--- podripper.sh
+++ podripper.sh
@@ -60,33 +60,34 @@ END_TIMESTAMP="${END_TIMESTAMP:-$( "$DATE" -d "+ ${DURATION_SEC} seconds" '+%s' 
 STREAM_IS_LIVE=
 
 while (( $( "$DATE" '+%s' ) < "$END_TIMESTAMP" )); do
-  STATUS="$(curl -sS https://atp.fm/livestream_status)"
-  echo "$STATUS"
+  if [[ "$1" == atp ]]; then
+    STATUS="$(curl -sS https://atp.fm/livestream_status)"
+    echo "$STATUS"
 
-  if [[ -z "$STREAM_IS_LIVE" ]]; then
-    # no live stream yet
-    if [[ "$1" == atp ]]; then
+    if [[ -z "$STREAM_IS_LIVE" ]]; then
+      # no live stream yet
       if jq -e .live <<< "$STATUS" >/dev/null; then
         STREAM_IS_LIVE=1
+
+        # try to parse the stream url from the status response
+        ORIG_STREAM_URL="$STREAM_URL"
+        PLAYER="$( jq -r .player <<< "$STATUS" || true )"
+        STREAM_URL="$( htmlq -a src 'audio source' <<< "$PLAYER" || true )"
+        echo "  0 stream url (audio source): $STREAM_URL"
+        [[ -z "$STREAM_URL" ]] && STREAM_URL="$( htmlq -a src audio <<< "$PLAYER" || true )"
+        echo "  1 stream url (audio): $STREAM_URL"
+        [[ -z "$STREAM_URL" ]] && STREAM_URL="$( sed -nE 's/.*"(http[^"]+)".*/\1/p' <<< "$PLAYER" || true )"
+        echo "  2 stream url (sed): $STREAM_URL"
+        [[ -z "$STREAM_URL" ]] && STREAM_URL="$ORIG_STREAM_URL"
+        echo "  3 stream url (original): $STREAM_URL"
       fi
     fi
+  else
+    STREAM_IS_LIVE=1
   fi
 
   if [[ -n "$STREAM_IS_LIVE" ]]; then
     echo "starting the ripper"
-    if [[ "$1" == atp ]]; then
-      # try to parse the stream url from the status response
-      ORIG_STREAM_URL="$STREAM_URL"
-      PLAYER="$( jq -r .player <<< "$STATUS" || true )"
-      STREAM_URL="$( htmlq -a src 'audio source' <<< "$PLAYER" || true )"
-      echo "  0 stream url (audio source): $STREAM_URL"
-      [[ -z "$STREAM_URL" ]] && STREAM_URL="$( htmlq -a src audio <<< "$PLAYER" || true )"
-      echo "  1 stream url (audio): $STREAM_URL"
-      [[ -z "$STREAM_URL" ]] && STREAM_URL="$( sed -nE 's/.*"(http[^"]+)".*/\1/p' <<< "$PLAYER" || true )"
-      echo "  2 stream url (sed): $STREAM_URL"
-      [[ -z "$STREAM_URL" ]] && STREAM_URL="$ORIG_STREAM_URL"
-      echo "  3 stream url (original): $STREAM_URL"
-    fi
     # TODO the loop to restart ripper is unnecessary because the program itself
     # should run for `$DURATION_SEC`
     /usr/bin/ripper-exe ripper --verbose -d "$RAW_RIP_DIR" -l "$DURATION_SEC" -r "$RETRY_SEC" "$STREAM_URL" || true
