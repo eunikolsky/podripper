835734cb30eef7cf7d314ee3b630dbf084bc8e64
Eugene <e@egeek.me>
2023-08-19 13:53:56 +0300

    Comment the hardcoded ATP support
    This will be ported after the main functionality.

--- podripper.sh
+++ podripper.sh
@@ -10,8 +10,6 @@ case "$OSTYPE" in
 esac
 
 # These environment variables can override the default values for debugging:
-# * `END_TIMESTAMP` -- when to stop recording.
-#     Note: you can set `END_TIMESTAMP=0` in order to skip the ripping step.
 # * `RIPPER` -- path to the `ripper-exe` binary.
 
 STREAM_NAME="$1"
@@ -61,48 +59,48 @@ STREAM_IS_LIVE=
 
 wait_for_stream() {
   # at the start, figure out the duration until which keep on waiting for the stream
-  END_TIMESTAMP="${END_TIMESTAMP:-$( "$DATE" -d "+ ${DURATION_SEC} seconds" '+%s' )}"
+  #END_TIMESTAMP="${END_TIMESTAMP:-$( "$DATE" -d "+ ${DURATION_SEC} seconds" '+%s' )}"
 
-  while (( $( "$DATE" '+%s' ) < "$END_TIMESTAMP" )); do
+  #while (( $( "$DATE" '+%s' ) < "$END_TIMESTAMP" )); do
     # FIXME the `atp` support is hardcoded in the program because its live
     # stream check is more complicated and the stream URL needs to be extracted
     # from the status endpoint; implementing a DSL in `conf` files and shell
     # isn't easy, so this should be more easily done when the script is
     # rewritten in Haskell
-    if [[ "$STREAM_NAME" == atp ]]; then
-      STATUS="$(curl -sS https://atp.fm/livestream_status)"
-      echo "$STATUS"
+    #if [[ "$STREAM_NAME" == atp ]]; then
+      #STATUS="$(curl -sS https://atp.fm/livestream_status)"
+      #echo "$STATUS"
 
-      if [[ -z "$STREAM_IS_LIVE" ]]; then
-        # no live stream yet
-        if jq -e .live <<< "$STATUS" >/dev/null; then
-          STREAM_IS_LIVE=1
+      #if [[ -z "$STREAM_IS_LIVE" ]]; then
+        ## no live stream yet
+        #if jq -e .live <<< "$STATUS" >/dev/null; then
+          #STREAM_IS_LIVE=1
 
-          # try to parse the stream url from the status response
-          ORIG_STREAM_URL="$STREAM_URL"
-          PLAYER="$( jq -r .player <<< "$STATUS" || true )"
-          # this overwrites the global variable
-          STREAM_URL="$( htmlq -a src 'audio source' <<< "$PLAYER" || true )"
-          echo "  0 stream url (audio source): $STREAM_URL"
-          [[ -z "$STREAM_URL" ]] && STREAM_URL="$( htmlq -a src audio <<< "$PLAYER" || true )"
-          echo "  1 stream url (audio): $STREAM_URL"
-          [[ -z "$STREAM_URL" ]] && STREAM_URL="$( sed -nE 's/.*"(http[^"]+)".*/\1/p' <<< "$PLAYER" || true )"
-          echo "  2 stream url (sed): $STREAM_URL"
-          [[ -z "$STREAM_URL" ]] && STREAM_URL="$ORIG_STREAM_URL"
-          echo "  3 stream url (original): $STREAM_URL"
-        fi
-      fi
-    else
+          ## try to parse the stream url from the status response
+          #ORIG_STREAM_URL="$STREAM_URL"
+          #PLAYER="$( jq -r .player <<< "$STATUS" || true )"
+          ## this overwrites the global variable
+          #STREAM_URL="$( htmlq -a src 'audio source' <<< "$PLAYER" || true )"
+          #echo "  0 stream url (audio source): $STREAM_URL"
+          #[[ -z "$STREAM_URL" ]] && STREAM_URL="$( htmlq -a src audio <<< "$PLAYER" || true )"
+          #echo "  1 stream url (audio): $STREAM_URL"
+          #[[ -z "$STREAM_URL" ]] && STREAM_URL="$( sed -nE 's/.*"(http[^"]+)".*/\1/p' <<< "$PLAYER" || true )"
+          #echo "  2 stream url (sed): $STREAM_URL"
+          #[[ -z "$STREAM_URL" ]] && STREAM_URL="$ORIG_STREAM_URL"
+          #echo "  3 stream url (original): $STREAM_URL"
+        #fi
+      #fi
+    #else
       STREAM_IS_LIVE=1
-    fi
+    #fi
 
     # if we've run out of time, no need to sleep one more time at the end
-    if (( $( "$DATE" -d "+ ${RETRY_SEC} seconds" '+%s' ) < "$END_TIMESTAMP" )); then
-      sleep "$RETRY_SEC"
-    else
-      break
-    fi
-  done
+    #if (( $( "$DATE" -d "+ ${RETRY_SEC} seconds" '+%s' ) < "$END_TIMESTAMP" )); then
+      #sleep "$RETRY_SEC"
+    #else
+      #break
+    #fi
+  #done
 }
 
 rip() {
