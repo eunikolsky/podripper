e03f484d9bdffbb65b506970c308bae66a5597bf
Eugene <e@egeek.me>
2023-10-14 17:49:47 +0300

    Load podripper config

--- src/Podripper.hs
+++ src/Podripper.hs
@@ -3,12 +3,31 @@ module Podripper
   , main
   ) where
 
+import Data.Aeson
+import Data.Maybe
 import Data.Text (Text)
 import qualified Data.Text as T
+import RipConfig
+import System.Environment
 import System.Exit (die)
+import System.FilePath
 
 type RipName = Text
 
 main :: RipName -> IO ()
--- FIXME remove `die` when the script has been migrated
-main ripName = die $ mconcat ["TODO implement ripping ", T.unpack ripName, "!"]
+main ripName = do
+  config <- loadConfig ripName
+  -- FIXME remove `die` when the script has been migrated
+  die $ "Loaded config: " <> show config
+
+loadConfig :: RipName -> IO RipConfig
+loadConfig ripName = do
+  confDir <- getConfDir
+  let confName = confDir </> T.unpack ripName <.> "json"
+  eitherConfig <- eitherDecodeFileStrict' @RipConfig confName
+  either die pure eitherConfig
+
+getConfDir :: IO FilePath
+getConfDir = do
+  maybeConfDir <- lookupEnv "CONF_DIR"
+  pure $ fromMaybe "/usr/share/podripper" maybeConfDir
