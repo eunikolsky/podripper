c5d82baeb6a0670bfea7c93b112a09aa88954ac4
Eugene <e@egeek.me>
2023-10-14 17:40:01 +0300

    Add a FIXME about `die`

--- src/Podripper.hs
+++ src/Podripper.hs
@@ -10,4 +10,5 @@ import System.Exit (die)
 type RipName = Text
 
 main :: RipName -> IO ()
+-- FIXME remove `die` when the script has been migrated
 main ripName = die $ mconcat ["TODO implement ripping ", T.unpack ripName, "!"]
