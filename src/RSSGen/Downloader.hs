ea36f91e5eb6967788ceb6151a865a85721ad76e
Eugene <e@egeek.me>
2023-10-30 08:11:58 +0200

    Replace `Response` with `DownloadResponse`

--- src/RSSGen/Downloader.hs
+++ src/RSSGen/Downloader.hs
@@ -1,5 +1,6 @@
 module RSSGen.Downloader
   ( Bytes
+  , DownloadResponse(..)
   , HTTPClientDownloadT(..)
   , MonadDownload(..)
   , URL
@@ -10,16 +11,32 @@ import Control.Monad.Catch
 import Control.Monad.Reader
 import qualified Data.ByteString.Lazy as BL
 import Network.HTTP.Client
-import Network.HTTP.Types.Status
+import Network.HTTP.Types
 
 type URL = String
 
 type Bytes = BL.ByteString
 
+-- | Contains the fields from `Network.HTTP.Client.Response` that are important
+-- for `MonadDownload` implementations. Creating values of this type doesn't
+-- require importing `Network.HTTP.Client.Internal`.
+data DownloadResponse = DownloadResponse
+  { drStatus :: !Status
+  , drHeaders :: !ResponseHeaders
+  , drBody :: !Bytes
+  }
+
+fromResponse :: Response Bytes -> DownloadResponse
+fromResponse r = DownloadResponse
+  { drStatus = responseStatus r
+  , drHeaders = responseHeaders r
+  , drBody = responseBody r
+  }
+
 -- |The API to download files via HTTP(S).
 class Monad m => MonadDownload m where
   -- |Downloads a file by the @URL@.
-  getFile :: URL -> m (Response Bytes)
+  getFile :: URL -> m DownloadResponse
 
 
 -- |A downloader that uses `Network.HTTP.Client`.
@@ -30,12 +47,13 @@ instance (MonadIO m, MonadThrow m) => MonadDownload (HTTPClientDownloadT m) wher
   getFile url = do
     manager <- ask
     request <- parseRequest url
-    liftIO $ httpLbs request manager
+    response <- liftIO $ httpLbs request manager
+    pure $ fromResponse response
 
 -- | Returns the `response`'s body if the download is successful, and `Nothing`
 -- otherwise.
-successfulBody :: Response a -> Maybe a
+successfulBody :: DownloadResponse -> Maybe Bytes
 successfulBody response =
-  if responseStatus response == ok200
-    then Just $ responseBody response
+  if drStatus response == ok200
+    then Just $ drBody response
     else Nothing
--- test/RSSGen/PollHTTPSpec.hs
+++ test/RSSGen/PollHTTPSpec.hs
@@ -2,9 +2,7 @@ module RSSGen.PollHTTPSpec where
 
 import Data.ByteString.Lazy.Char8 qualified as CL
 import Data.Functor.Identity
-import Network.HTTP.Client.Internal (Response(..), ResponseClose(..))
 import Network.HTTP.Types.Status
-import Network.HTTP.Types.Version
 import RSSGen.Downloader
 import RSSGen.PollHTTP
 import Test.Hspec
@@ -20,12 +18,8 @@ newtype FakeDownloadT m a = FakeDownloadT { runFakeDownloadT :: m a }
   deriving (Functor, Applicative, Monad)
 
 instance Monad m => MonadDownload (FakeDownloadT m) where
-  getFile url = pure $ Response
-    { responseStatus = ok200
-    , responseBody = "<!-- " <> CL.pack url <> " -->"
-    , responseVersion = http11
-    , responseHeaders = mempty
-    , responseCookieJar = mempty
-    , responseClose' = ResponseClose mempty
-    , responseOriginalRequest = undefined
+  getFile url = pure $ DownloadResponse
+    { drStatus = ok200
+    , drBody = "<!-- " <> CL.pack url <> " -->"
+    , drHeaders = mempty
     }
