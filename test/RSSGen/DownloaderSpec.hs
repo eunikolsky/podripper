27fe87a675ce60cd90f97185c97e4410ca53439e
Eugene <e@egeek.me>
2023-11-07 07:12:25 +0200

    Group tests by response code

--- test/RSSGen/DownloaderSpec.hs
+++ test/RSSGen/DownloaderSpec.hs
@@ -15,127 +15,129 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "getFile" $ do
-    let verifyStored item = do
-          let url = "http://localhost"
-              mockHTTPBS _ = pure $ responseWith item
+    context "given 200 OK response" $ do
+      let verifyStored item = do
+            let url = "http://localhost"
+                mockHTTPBS _ = pure $ responseWith item
 
-          actual <- withDB $ \conn -> do
-            void $ getFile mockHTTPBS conn url
-            liftIO $ getCacheItem conn url
+            actual <- withDB $ \conn -> do
+              void $ getFile mockHTTPBS conn url
+              liftIO $ getCacheItem conn url
 
-          actual `shouldBe` Just item
+            actual `shouldBe` Just item
 
-    it "stores ETag from a response" $ verifyStored $ ETag "etag"
+      it "stores ETag from a response" $ verifyStored $ ETag "etag"
 
-    it "stores Last-Modified from a response" $ verifyStored $ LastModified "last-modified"
+      it "stores Last-Modified from a response" $ verifyStored $ LastModified "last-modified"
 
-    it "stores ETag and Last-Modified from a response" $ verifyStored $
-      ETagWithLastModified "etag" "last-modified"
+      it "stores ETag and Last-Modified from a response" $ verifyStored $
+        ETagWithLastModified "etag" "last-modified"
 
-    it "stores Body of a response" $ do
-      let url = "http://localhost"
-          body = Body "response body"
-          mockHTTPBS _ = pure $ responseWith body
+      it "stores Body of a response" $ do
+        let url = "http://localhost"
+            body = Body "response body"
+            mockHTTPBS _ = pure $ responseWith body
 
-      actual <- withDB $ \conn -> do
-        void $ getFile mockHTTPBS conn url
-        liftIO $ getCacheItem conn url
+        actual <- withDB $ \conn -> do
+          void $ getFile mockHTTPBS conn url
+          liftIO $ getCacheItem conn url
 
-      actual `shouldBe` Just body
+        actual `shouldBe` Just body
 
-    it "sets If-Modified-Since with stored ETag" $ do
-      ifModifiedSinceRef <- newIORef Nothing
+      it "sets If-Modified-Since with stored ETag" $ do
+        ifModifiedSinceRef <- newIORef Nothing
 
-      let url = "http://localhost"
-          etag = ETag "etag"
-          mockHTTPBS req = do
-            writeIORef ifModifiedSinceRef .
-              findHeaderValue "If-Modified-Since" $ requestHeaders req
-            pure $ responseWith etag
+        let url = "http://localhost"
+            etag = ETag "etag"
+            mockHTTPBS req = do
+              writeIORef ifModifiedSinceRef .
+                findHeaderValue "If-Modified-Since" $ requestHeaders req
+              pure $ responseWith etag
 
-      withDB $ \conn -> do
-        liftIO $ setCacheItem conn url etag
-        void $ getFile mockHTTPBS conn url
+        withDB $ \conn -> do
+          liftIO $ setCacheItem conn url etag
+          void $ getFile mockHTTPBS conn url
 
-      actual <- readIORef ifModifiedSinceRef
-      ETag <$> actual `shouldBe` Just etag
+        actual <- readIORef ifModifiedSinceRef
+        ETag <$> actual `shouldBe` Just etag
 
-    it "sets If-None-Match with stored LastModified" $ do
-      ifNoneMatchRef <- newIORef Nothing
+      it "sets If-None-Match with stored LastModified" $ do
+        ifNoneMatchRef <- newIORef Nothing
 
-      let url = "http://localhost"
-          lastmod = LastModified "last-modified"
-          mockHTTPBS req = do
-            writeIORef ifNoneMatchRef .
-              findHeaderValue "If-None-Match" $ requestHeaders req
-            pure $ responseWith lastmod
+        let url = "http://localhost"
+            lastmod = LastModified "last-modified"
+            mockHTTPBS req = do
+              writeIORef ifNoneMatchRef .
+                findHeaderValue "If-None-Match" $ requestHeaders req
+              pure $ responseWith lastmod
 
-      withDB $ \conn -> do
-        liftIO $ setCacheItem conn url lastmod
-        void $ getFile mockHTTPBS conn url
+        withDB $ \conn -> do
+          liftIO $ setCacheItem conn url lastmod
+          void $ getFile mockHTTPBS conn url
 
-      actual <- readIORef ifNoneMatchRef
-      LastModified <$> actual `shouldBe` Just lastmod
+        actual <- readIORef ifNoneMatchRef
+        LastModified <$> actual `shouldBe` Just lastmod
 
-    it "sets Is-Modified-Since and If-None-Match with stored ETagWithLastModified" $ do
-      headerValuesRef <- newIORef Nothing
+      it "sets Is-Modified-Since and If-None-Match with stored ETagWithLastModified" $ do
+        headerValuesRef <- newIORef Nothing
 
-      let url = "http://localhost"
-          etagLastmod = ETagWithLastModified "etag" "last-modified"
-          mockHTTPBS req = do
-            writeIORef headerValuesRef $ sequence
-              ( findHeaderValue "If-Modified-Since" $ requestHeaders req
-              , findHeaderValue "If-None-Match" $ requestHeaders req
-              )
-            pure $ responseWith etagLastmod
+        let url = "http://localhost"
+            etagLastmod = ETagWithLastModified "etag" "last-modified"
+            mockHTTPBS req = do
+              writeIORef headerValuesRef $ sequence
+                ( findHeaderValue "If-Modified-Since" $ requestHeaders req
+                , findHeaderValue "If-None-Match" $ requestHeaders req
+                )
+              pure $ responseWith etagLastmod
 
-      withDB $ \conn -> do
-        liftIO $ setCacheItem conn url etagLastmod
-        void $ getFile mockHTTPBS conn url
+        withDB $ \conn -> do
+          liftIO $ setCacheItem conn url etagLastmod
+          void $ getFile mockHTTPBS conn url
 
-      actual <- readIORef headerValuesRef
-      let (ifModifiedSince, ifNoneMatch) = sequence actual
-      ETagWithLastModified <$> ifModifiedSince <*> ifNoneMatch `shouldBe` Just etagLastmod
+        actual <- readIORef headerValuesRef
+        let (ifModifiedSince, ifNoneMatch) = sequence actual
+        ETagWithLastModified <$> ifModifiedSince <*> ifNoneMatch `shouldBe` Just etagLastmod
 
-    it "returns body when response Body has changed from cached" $ do
-      let url = "http://localhost"
-          cachedBody = Body "previous body"
-          body = "response body"
-          mockHTTPBS _ = pure $ responseWith (Body body)
+      it "returns body when response Body has changed from cached" $ do
+        let url = "http://localhost"
+            cachedBody = Body "previous body"
+            body = "response body"
+            mockHTTPBS _ = pure $ responseWith (Body body)
 
-      actual <- withDB $ \conn -> do
-        liftIO $ setCacheItem conn url cachedBody
-        getFile mockHTTPBS conn url
+        actual <- withDB $ \conn -> do
+          liftIO $ setCacheItem conn url cachedBody
+          getFile mockHTTPBS conn url
 
-      actual `shouldBe` Just body
+        actual `shouldBe` Just body
 
-    it "returns body when response Body wasn't cached" $ do
-      let url = "http://localhost"
-          body = "response body"
-          mockHTTPBS _ = pure $ responseWith (Body body)
+      it "returns body when response Body wasn't cached" $ do
+        let url = "http://localhost"
+            body = "response body"
+            mockHTTPBS _ = pure $ responseWith (Body body)
 
-      actual <- withDB $ \conn -> getFile mockHTTPBS conn url
+        actual <- withDB $ \conn -> getFile mockHTTPBS conn url
 
-      actual `shouldBe` Just body
+        actual `shouldBe` Just body
 
-    it "returns Nothing when response Body hasn't changed from cached" $ do
-      let url = "http://localhost"
-          body = Body "response body"
-          mockHTTPBS _ = pure $ responseWith body
+      it "returns Nothing when response Body hasn't changed from cached" $ do
+        let url = "http://localhost"
+            body = Body "response body"
+            mockHTTPBS _ = pure $ responseWith body
 
-      actual <- withDB $ \conn -> do
-        liftIO $ setCacheItem conn url body
-        getFile mockHTTPBS conn url
+        actual <- withDB $ \conn -> do
+          liftIO $ setCacheItem conn url body
+          getFile mockHTTPBS conn url
 
-      actual `shouldBe` Nothing
+        actual `shouldBe` Nothing
 
-    it "returns Nothing when response is 304 Not modified" $ do
-      let url = "http://localhost"
-          mockHTTPBS _ = pure responseNotModified
+    context "given 304 Not modified response" $ do
+      it "returns Nothing" $ do
+        let url = "http://localhost"
+            mockHTTPBS _ = pure responseNotModified
 
-      actual <- withDB $ \conn -> getFile mockHTTPBS conn url
+        actual <- withDB $ \conn -> getFile mockHTTPBS conn url
 
-      actual `shouldBe` Nothing
+        actual `shouldBe` Nothing
 
 responseWith :: CacheItem -> Response Bytes
 responseWith item = Response
