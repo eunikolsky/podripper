38642364d2635f19e3aff5ca8467fd24e2061f0a
Eugene <e@egeek.me>
2023-11-11 21:12:38 +0200

    Fix out of order and delayed logs when running with `systemd`

--- app/Main.hs
+++ app/Main.hs
@@ -24,6 +24,8 @@ main = do
         )
   progOptions <- execParser opts
 
+  setLineBuffering
+
   case progOptions of
     RunOptions ripName -> Podripper.run ripName
     RipperOptions options -> Ripper.run options
@@ -46,3 +48,21 @@ data ProgramOptions
   = RunOptions Run.RipName
   | RipperOptions Options
   | RSSGenOptions (NonEmpty FilePath)
+
+-- | Sets the line buffering mode for `stdout` and `stderr` to make sure log
+-- lines are displayed promptly and in order.
+--
+-- The issue is that podripper's logs weren't always in the correct time
+-- sequence — this only happened when running via `systemd`. For radiot and
+-- rcmp, there were first ripper's logs, then the wrapper's (podripper's) logs,
+-- that is the "starting the ripper" message appeared after the multi-hour
+-- ripper was done; for atp, the podripper's logs did appear first, but only in
+-- blocks (visible by systemd's timestamps), and they were interrupted by the
+-- entirety of the ripper's logs. ripper uses a proper logger into `stderr`,
+-- whereas podripper uses plain logging to `stdout`.
+--
+-- See also: https://stackoverflow.com/questions/19520885/haskell-default-io-buffering.
+--
+-- FIXME use a proper logger for the entire program
+setLineBuffering :: IO ()
+setLineBuffering = forM_ [stdout, stderr] $ flip hSetBuffering LineBuffering
