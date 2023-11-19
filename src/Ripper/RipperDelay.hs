d78fad6886852bfa29c4333a226ed30d65c1986e
Eugene <e@egeek.me>
2023-11-19 15:59:53 +0200

    Return fixed delay instead a ripper interval

--- src/Ripper/RipperDelay.hs
+++ src/Ripper/RipperDelay.hs
@@ -1,18 +1,51 @@
 module Ripper.RipperDelay
-  ( getRipperDelay
+  ( RipperInterval
+  , getRipperDelay
+  , mkRipperInterval
   ) where
 
+import Data.Time
+import Data.Time.TZInfo
 import Data.Time.TZTime
 import RSSGen.Duration
+
+-- | Timezone — wrapper around `TZInfo` that `show`s only the identifier
+-- (without a long list of rules).
+newtype TZ = TZ { toTZInfo :: TZInfo }
+
+instance Show TZ where
+  show = show . tziIdentifier . toTZInfo
+
+data RipperInterval = RipperInterval
+  { riWeekday :: !DayOfWeek
+  , riTimeInterval :: !(TimeOfDay, TimeOfDay)
+  -- ^ `from` is always less than `to`
+  , riTZ :: !TZ
+  }
+  deriving Show
+
+mkRipperInterval :: DayOfWeek -> (TimeOfDay, TimeOfDay) -> TZInfo -> Maybe RipperInterval
+mkRipperInterval d ti@(from, to) tz = if from < to then Just (RipperInterval d ti $ TZ tz) else Nothing
 
 type RipEndTime = TZTime
 type Now = TZTime
 
-getRipperDelay :: Maybe RipEndTime -> Now -> RetryDelay
-getRipperDelay (Just ripEndTime) now = if now `diffTZTime` ripEndTime <= minutes 5
+getRipperDelay :: RipperInterval -> Maybe RipEndTime -> Now -> RetryDelay
+getRipperDelay _ (Just ripEndTime) now = if now `diffTZTime` ripEndTime <= minutes 5
   then shortAfterRipDelay
   else longerAfterRipDelay
-getRipperDelay Nothing _ = defaultDelay
+getRipperDelay interval Nothing localNow = if nowWithinInterval then standardIntervalDelay else defaultDelay
+  where
+    nowWithinInterval = nextWeekdayIsToday && nowWithinTimeInterval
+    nextWeekdayIsToday = firstDayOfWeekOnAfter (riWeekday interval) today == today
+    nowWithinTimeInterval = localTimeOfDay now `between` riTimeInterval interval
+    today = localDay now
+    -- this is the passed `localNow` (in the program's current timezone)
+    -- converted to the interval's timezone
+    now = tzTimeLocalTime $ inTZ (toTZInfo $ riTZ interval) localNow
+
+between :: Ord a => a -> (a, a) -> Bool
+x `between` (from, to) = x >= from && x <= to
 
 -- A very short ripper delay that happens soon after a rip has ended because we
 -- don't know whether the stream has ended or there was an error and it will be
@@ -22,6 +55,9 @@ shortAfterRipDelay = RetryDelay $ durationSeconds 1
 
 longerAfterRipDelay :: RetryDelay
 longerAfterRipDelay = RetryDelay $ durationSeconds 3
+
+standardIntervalDelay :: RetryDelay
+standardIntervalDelay = RetryDelay $ durationSeconds 20
 
 -- | The default ripper delay if no other rule matches.
 defaultDelay :: RetryDelay
--- test/Ripper/RipperDelaySpec.hs
+++ test/Ripper/RipperDelaySpec.hs
@@ -1,9 +1,14 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Ripper.RipperDelaySpec where
 
+import Data.Maybe
+import Data.Time
 import Data.Time.Calendar.OrdinalDate
-import Data.Time.LocalTime
 import Data.Time.TZInfo
 import Data.Time.TZTime
+import Data.Time.TZTime.QQ
 import RSSGen.Duration
 import Ripper.RipperDelay
 import Test.Hspec
@@ -14,19 +19,33 @@ spec :: Spec
 spec = do
   describe "getRipperDelay" $ do
     context "after ripping ended" $ do
-      prop "returns 1 s within 5 minutes" $ \(Now now) -> do
+      prop "returns 1 s within 5 minutes" $ \interval (Now now) -> do
         offset <- realToFrac <$> choose @Float (0, 5 * 60)
         let ripEndTime = addTime (negate offset) now
-        pure $ getRipperDelay (Just ripEndTime) now == RetryDelay (durationSeconds 1)
+        pure $ getRipperDelay interval (Just ripEndTime) now == RetryDelay (durationSeconds 1)
 
-      prop "returns 3 s within 15 minutes" $ \(Now now) -> do
+      prop "returns 3 s within 15 minutes" $ \interval (Now now) -> do
         offset <- realToFrac <$> choose @Float ((5 * 60) + 1, 15 * 60)
         let ripEndTime = addTime (negate offset) now
-        pure $ getRipperDelay (Just ripEndTime) now == RetryDelay (durationSeconds 3)
+        pure $ getRipperDelay interval (Just ripEndTime) now == RetryDelay (durationSeconds 3)
 
-    context "without previous ripping" $
-      prop "returns default delay" $ \(Now now) ->
-        getRipperDelay Nothing now == RetryDelay (durationMinutes 10)
+    context "without previous ripping" $ do
+      let newYork = fromLabel America__New_York
+      let interval = fromJust $ mkRipperInterval Wednesday (read "20:00:00", read "22:00:00") newYork
+
+      it "returns default delay when outside time interval" $ do
+        let now = [tz|2023-11-19 04:00:00 [Europe/Kyiv]|] -- Sunday
+        getRipperDelay interval Nothing now `shouldBe` RetryDelay (durationMinutes 10)
+
+      it "returns fixed delay when inside time interval" $ do
+        let now = [tz|2023-11-16 04:00:00 [Europe/Kyiv]|] -- Thursday
+        getRipperDelay interval Nothing now `shouldBe` RetryDelay (durationSeconds 20)
+
+instance Arbitrary TimeOfDay where
+  arbitrary = TimeOfDay
+    <$> chooseInt (0, 23)
+    <*> chooseInt (0, 59)
+    <*> (realToFrac <$> chooseInt (0, 59))
 
 newtype Now = Now TZTime
   deriving newtype (Show)
@@ -36,10 +55,16 @@ instance Arbitrary Now where
     day <- fromOrdinalDate
       <$> chooseInteger (2023, 2123)
       <*> chooseInt (1, 365)
-    timeOfDay <- TimeOfDay
-      <$> chooseInt (0, 23)
-      <*> chooseInt (0, 59)
-      <*> (realToFrac <$> chooseInt (0, 59))
+    timeOfDay <- arbitrary
     let time = LocalTime day timeOfDay
     let tzInfo = fromLabel Europe__Kyiv
     pure . Now $ fromLocalTime tzInfo time
+
+instance Arbitrary RipperInterval where
+  arbitrary = do
+    day <- chooseEnum (Monday, Sunday)
+    from <- arbitrary
+    to <- arbitrary
+    let from' = min from to
+        to' = max from to
+    pure . fromJust $ mkRipperInterval day (from', to') $ fromLabel America__Anchorage
