8dcb6c9629fbfcc6add8034497d1c30a76419cd5
Eugene <e@egeek.me>
2023-11-19 09:08:14 +0200

    Return 1 s delay after ripping

--- src/Ripper/RipperDelay.hs
+++ src/Ripper/RipperDelay.hs
@@ -2,10 +2,21 @@ module Ripper.RipperDelay
   ( getRipperDelay
   ) where
 
+import Data.Time.TZTime
 import RSSGen.Duration
 
-getRipperDelay :: RetryDelay
-getRipperDelay = defaultDelay
+type RipEndTime = TZTime
+type Now = TZTime
+
+getRipperDelay :: Maybe RipEndTime -> Now -> RetryDelay
+getRipperDelay (Just _) _ = shortAfterRipDelay
+getRipperDelay Nothing _ = defaultDelay
+
+-- A very short ripper delay that happens soon after a rip has ended because we
+-- don't know whether the stream has ended or there was an error and it will be
+-- back soon.
+shortAfterRipDelay :: RetryDelay
+shortAfterRipDelay = RetryDelay $ durationSeconds 1
 
 -- | The default ripper delay if no other rule matches.
 defaultDelay :: RetryDelay
--- test/Ripper/RipperDelaySpec.hs
+++ test/Ripper/RipperDelaySpec.hs
@@ -1,11 +1,40 @@
 module Ripper.RipperDelaySpec where
 
+import Data.Time.Calendar.OrdinalDate
+import Data.Time.LocalTime
+import Data.Time.TZInfo
+import Data.Time.TZTime
 import RSSGen.Duration
 import Ripper.RipperDelay
 import Test.Hspec
+import Test.Hspec.QuickCheck
+import Test.QuickCheck
 
 spec :: Spec
 spec = do
   describe "getRipperDelay" $ do
-    it "returns default delay" $
-      getRipperDelay `shouldBe` RetryDelay (durationMinutes 10)
+    context "after ripping ended" $ do
+      prop "returns 1 s within 5 minutes" $ \(Now now) -> do
+        offset <- realToFrac <$> choose @Float (0, 5 * 60)
+        let ripEndTime = addTime (negate offset) now
+        pure $ getRipperDelay (Just ripEndTime) now == RetryDelay (durationSeconds 1)
+
+    context "without previous ripping" $
+      prop "returns default delay" $ \(Now now) ->
+        getRipperDelay Nothing now == RetryDelay (durationMinutes 10)
+
+newtype Now = Now TZTime
+  deriving newtype (Show)
+
+instance Arbitrary Now where
+  arbitrary = do
+    day <- fromOrdinalDate
+      <$> chooseInteger (2023, 2123)
+      <*> chooseInt (1, 365)
+    timeOfDay <- TimeOfDay
+      <$> chooseInt (0, 23)
+      <*> chooseInt (0, 59)
+      <*> (realToFrac <$> chooseInt (0, 59))
+    let time = LocalTime day timeOfDay
+    let tzInfo = fromLabel Europe__Kyiv
+    pure . Now $ fromLocalTime tzInfo time
