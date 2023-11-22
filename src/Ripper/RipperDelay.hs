f20b32db94ae4b1fffa36a2389f38c0c16c9483b
Eugene <e@egeek.me>
2023-11-22 06:51:17 +0200

    Add comments in `Ripper.RipperDelay`

--- src/Ripper/RipperDelay.hs
+++ src/Ripper/RipperDelay.hs
@@ -18,6 +18,13 @@ newtype TZ = TZ { toTZInfo :: TZInfo }
 instance Show TZ where
   show = show . tziIdentifier . toTZInfo
 
+-- | Time interval that provides a specific ripper delay, overriding the default
+-- delay. It's used for podcasts that have known live recording times and we
+-- want to record them from the beginning. Since they are usually weekly, this
+-- interval is based on a time-of-day interval in the given timezone on a
+-- certain day of week.
+-- Note that the time interval currently isn't expected to overflow to the next
+-- day, that is `(23:00:00, 01:30:00)` is unsupported.
 data RipperInterval = RipperInterval
   { riWeekday :: !DayOfWeek
   , riTimeInterval :: !(TimeOfDay, TimeOfDay)
@@ -28,12 +35,28 @@ data RipperInterval = RipperInterval
   }
   deriving Show
 
+-- | Smart constructor for `RipperInterval` that ensures that the time interval
+-- is ascending (`from ≥ to`).
 mkRipperInterval :: DayOfWeek -> (TimeOfDay, TimeOfDay) -> TZInfo -> RetryDelay -> Maybe RipperInterval
 mkRipperInterval d ti@(from, to) tz delay = if from < to then Just (RipperInterval d ti (TZ tz) delay) else Nothing
 
 type RipEndTime = TZTime
 type Now = TZTime
 
+-- | Determines the delay before the next ripping attempt. The rules are:
+-- * within 5 minutes after latest rip ended => 1 second;
+-- * within 15 minutes after latest rip ended => 3 seconds;
+-- * if `now` is inside a ripper interval => use its specific delay;
+-- * otherwise => 10 minutes, but so that it's not longer than the time to
+-- the next ripper interval [0].
+--
+-- [0] Why? Assuming the default delay to be quite big (10 minutes) and an
+-- interval delay to be small (10 seconds) because a live podcast starts on
+-- time (imagine that!), asking for a delay right before the interval start
+-- would return the big delay, missing the start. This avoids that by
+-- returning a delay only big enough to wake up after the interval start.
+-- Why is only the default delay limited? Because the assumption is that
+-- it's big and all other delays are much smaller.
 getRipperDelay :: [RipperInterval] -> Maybe RipEndTime -> Now -> RetryDelay
 getRipperDelay intervals (Just ripEndTime) now
   | timeSinceRipEnd <= minutes 5 = shortAfterRipDelay
@@ -58,15 +81,6 @@ getRipperDelay intervals Nothing localNow = maybe limitedDefaultDelay riDelay $ 
     -- converted to the interval's timezone
     tzNowInIntervalTZ interval = inTZ (toTZInfo $ riTZ interval) localNow
 
-    -- This limits the default delay so that it's not longer than the time to
-    -- the next ripper interval.
-    -- Why? Assuming the default delay to be quite big (10 minutes) and an
-    -- interval delay to be small (10 seconds) because a live podcast starts on
-    -- time (imagine that!), asking for a delay right before the interval start
-    -- would return the big delay, missing the start. This avoids that by
-    -- returning a delay only big enough to wake up after the interval start.
-    -- Why is only the default delay limited? Because the assumption is that
-    -- it's big and all other delays are much smaller.
     limitedDefaultDelay = defaultDelay `noBiggerThan` delayToNextInterval
     delayToNextInterval = RetryDelay . durationSeconds . ceiling $ diffTimeToNextInterval + 1
     diffTimeToNextInterval = minimum $ fmap delayToInterval intervals
@@ -92,12 +106,13 @@ x `between` (from, to) = x >= from && x <= to
 noBiggerThan :: Ord a => a -> a -> a
 noBiggerThan = min
 
--- A very short ripper delay that happens soon after a rip has ended because we
--- don't know whether the stream has ended or there was an error and it will be
--- back soon.
+-- A very short ripper delay soon after a rip has ended because we don't know
+-- whether the stream has ended or there was an error and it will be back soon.
 shortAfterRipDelay :: RetryDelay
 shortAfterRipDelay = RetryDelay $ durationSeconds 1
 
+-- A longer ripper delay after a rip has ended that may still catch some live
+-- stream.
 longerAfterRipDelay :: RetryDelay
 longerAfterRipDelay = RetryDelay $ durationSeconds 3
 
